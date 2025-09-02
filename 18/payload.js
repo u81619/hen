4642d5e72da4a7b40f0a4d9645a04b82ec9080b7
u81619 <haider13ahmed2@gmail.com>
2025-09-02 17:43:48 +0300

    Add files via upload

---
+++ 18/payload.js
@@ -0,0 +1,44 @@
+function load_gtabeef138(){
+LoadedMSG="Installer gta5 rdr2 prx Loaded !!!";
+LoadFromGHBLS('installergta5rdr2prx.bin');
+}
+
+function load_dumper(){
+LoadedMSG="Dumper v2 Loaded !!!";
+LoadFromGHBLS('gamedumper.bin');
+}
+
+function load_ps4debug(){
+LoadedMSG="PS4Debug v1.0.15  Loaded !!!";
+LoadFromGHBLS('ps4debug.bin');
+}
+
+function load_cacheinstall(){
+LoadedMSG="restore DB Loaded !!!";
+LoadFromGHBLS('restore.bin');
+}
+
+function load_cacheinstall2(){
+LoadedMSG="Backup DB Loaded !!!";
+LoadFromGHBLS('backup.bin');
+}
+
+function load_pkg(){
+LoadedMSG="Backup Pkg v1.1 Loaded!!! Load your game!!!";
+LoadFromGHBLS('pkgbackup.bin');
+}
+
+function load_disabled_update(){
+LoadedMSG="Disabled Update Loaded!!!";
+LoadFromGHBLS('disabled_update.bin');
+}
+
+function load_enabled_update(){
+LoadedMSG="Enabled Update Loaded!!!";
+LoadFromGHBLS('enabled_update.bin');
+}
+
+function load_fix(){
+LoadedMSG="psfree fix Aio Loaded!!!";
+LoadFromGHBLS('psfree-fix.bin');
+}
